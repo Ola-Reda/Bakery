96d45c128099263cc506fdf1483a3612e84616b8
unknown <ola.reda.900@gmail.com>
2023-09-06 02:48:57 +0300

    Add new changes

--- js/data.js
+++ js/data.js
@@ -3,72 +3,84 @@ let products = [
         id: 1,
         name: 'sweets',
         image: 'images/sweets-1.jpeg',
-        price: 5
+        price: 5, 
+        category: "sweets"
     },
     {
         id: 2,
         name: 'cupcake',
         image: 'images/cupcake-1.jpeg',
-        price: 7
+        price: 7,
+        category: "cupcake"
     },
     {
         id: 3,
         name: 'cake',
         image: 'images/cake-1.jpeg',
-        price: 10
+        price: 10,
+        category: "cake"
     },
     {
         id: 4,
         name: 'doughnut',
         image: 'images/doughnut-1.jpeg',
-        price: 8
+        price: 8,
+        category: "doughnut"
     },
     {
         id: 5,
         name: 'sweets',
         image: 'images/sweets-2.jpeg',
-        price: 4
+        price: 4,
+        category: "sweets"
     },
     {
         id: 6,
         name: 'cupcake',
         image: 'images/cupcake-2.jpeg',
-        price: 9
+        price: 9,
+        category: "cupcake"
     },
     {
         id: 7,
         name: 'cake',
         image: 'images/cake-2.jpeg',
-        price: 15
+        price: 15,
+        category: "cake"
     },
     {
         id: 8,
         name: 'doughnut',
         image: 'images/doughnut-2.jpeg',
-        price: 4
+        price: 4,
+        category: "doughnut"
     },
     {
         id: 9,
         name: 'sweets',
         image: 'images/sweets-3.jpeg',
-        price: 5
+        price: 5,
+        category: "sweets"
     },
     {
         id: 10,
         name: 'cupcake',
         image: 'images/cupcake-3.jpeg',
-        price: 12
+        price: 12,
+        category: "cupcake"
     },
     {
         id: 11,
         name: 'cake',
         image: 'images/cake-3.jpeg',
-        price: 30
+        price: 30,
+        category: "cake"
     },
     {
         id: 12,
         name: 'doughnut',
         image: 'images/doughnut-3.jpeg',
-        price: 12
+        price: 12,
+        category: "doughnut"
     }
 ];
--- js/script.js
+++ js/script.js
@@ -1,5 +1,96 @@
+let products = [
+    {
+        id: 1,
+        name: 'sweets',
+        image: 'images/sweets-1.jpeg',
+        price: 5, 
+        category: "sweets"
+    },
+    {
+        id: 2,
+        name: 'cupcake',
+        image: 'images/cupcake-1.jpeg',
+        price: 7,
+        category: "cupcake"
+    },
+    {
+        id: 3,
+        name: 'cake',
+        image: 'images/cake-1.jpeg',
+        price: 10,
+        category: "cake"
+    },
+    {
+        id: 4,
+        name: 'doughnut',
+        image: 'images/doughnut-1.jpeg',
+        price: 8,
+        category: "doughnut"
+    },
+    {
+        id: 5,
+        name: 'sweets',
+        image: 'images/sweets-2.jpeg',
+        price: 4,
+        category: "sweets"
+    },
+    {
+        id: 6,
+        name: 'cupcake',
+        image: 'images/cupcake-2.jpeg',
+        price: 9,
+        category: "cupcake"
+    },
+    {
+        id: 7,
+        name: 'cake',
+        image: 'images/cake-2.jpeg',
+        price: 15,
+        category: "cake"
+    },
+    {
+        id: 8,
+        name: 'doughnut',
+        image: 'images/doughnut-2.jpeg',
+        price: 4,
+        category: "doughnut"
+    },
+    {
+        id: 9,
+        name: 'sweets',
+        image: 'images/sweets-3.jpeg',
+        price: 5,
+        category: "sweets"
+    },
+    {
+        id: 10,
+        name: 'cupcake',
+        image: 'images/cupcake-3.jpeg',
+        price: 12,
+        category: "cupcake"
+    },
+    {
+        id: 11,
+        name: 'cake',
+        image: 'images/cake-3.jpeg',
+        price: 30,
+        category: "cake"
+    },
+    {
+        id: 12,
+        name: 'doughnut',
+        image: 'images/doughnut-3.jpeg',
+        price: 12,
+        category: "doughnut"
+    }
+];
 let box = document.querySelector('.store .box');
+let btns = document.querySelectorAll('ul li');
 
+console.log(products.keys())
+
+
+//add products to page
 let addProducts = function (products) {
     let draw = ''
     products.map(product => {
@@ -17,4 +108,67 @@ let addProducts = function (products) {
     })
     box.innerHTML = draw
 }
-addProducts(products)
+addProducts(products);
+
+
+// let card = document.querySelector('.card')
+// for( i of products){
+//     console.log(i.category)
+//     cards.forEach(card => {
+//             card.classList.add(i.category)        
+//     })
+// }
+
+// let cards = document.querySelectorAll('.card')
+// cards.forEach(card => {
+//     products.forEach(product => {
+//         card.classList.add(product.category)
+//     })
+    
+// })
+
+// products.forEach(product => {
+//     // console.log(product.category)
+//     cards.forEach(card => {
+//         // console.log(product)
+//         card.classList.add(product.category)
+//     })
+// })
+let cards = document.querySelectorAll('.card');
+// cards.forEach(product => {
+//     console.log(product)
+//     cat = product.classList.add("category")
+// })
+
+
+//filter products
+function filterProducts (value) {
+    btns.forEach(btn => {
+        if(value.toUpperCase() === btn.innerText.toUpperCase()) {
+            btn.classList.add('active')
+        }else {
+            btn.classList.remove('active')
+        }
+    })
+
+    cards.forEach(card => {
+        card.classList.add('hide')
+        if(value == 'all'){
+        card.classList.remove('hide')
+        }else {
+            if(card.classList.contains(value)) {
+                card.classList.remove('hide')
+            }else {
+                card.classList.remove('hide')
+            }
+        }
+    })
+    
+}
+
+window.onload = () => {
+    filterProducts('all')
+}
+
+//search products
+
