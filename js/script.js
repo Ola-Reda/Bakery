6730fca21737fa9dc5b5b04b107c9794fa1a8d7d
unknown <ola.reda.900@gmail.com>
2023-09-06 04:46:47 +0300

    Add new sections

--- js/script.js
+++ js/script.js
@@ -97,7 +97,10 @@ let addProducts = function (products) {
         draw += `
         <div class="col-md-4">
             <div class="card mt-3 mb-3">
-                <img src=${product.image} class="card-img-top" alt="image">
+                <div class="card-image">
+                    <img src=${product.image} class="card-img-top" alt="image">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                </div>
                 <div class="card-body d-flex align-items-center justify-content-between">
                     <h5 class="card-title text-capitalize">${product.name}</h5>
                     <p class="card-text">$${product.price}</p>
