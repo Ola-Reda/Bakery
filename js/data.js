070cab04b88e154b3fd84be1e853b3a75a2373b6
Ola-Reda <ola.reda.900@gmail.com>
2024-02-27 23:02:21 +0200

    Add changes in sections

--- js/data.js
+++ js/data.js
@@ -1,86 +1,104 @@
-let products = [
+const products = [
     {
         id: 1,
-        name: 'sweets',
+        name: 'macarons',
         image: 'images/sweets-1.jpeg',
         price: 5, 
         category: "sweets"
     },
     {
         id: 2,
-        name: 'cupcake',
+        name: 'vanilla cupcake',
         image: 'images/cupcake-1.jpeg',
         price: 7,
         category: "cupcake"
     },
     {
         id: 3,
-        name: 'cake',
+        name: 'gluten cake',
         image: 'images/cake-1.jpeg',
         price: 10,
         category: "cake"
     },
     {
         id: 4,
-        name: 'doughnut',
+        name: 'pink doughnut',
         image: 'images/doughnut-1.jpeg',
         price: 8,
         category: "doughnut"
     },
     {
         id: 5,
-        name: 'sweets',
+        name: 'candy',
         image: 'images/sweets-2.jpeg',
         price: 4,
         category: "sweets"
     },
     {
         id: 6,
-        name: 'cupcake',
+        name: 'choc cupcake',
         image: 'images/cupcake-2.jpeg',
         price: 9,
         category: "cupcake"
     },
     {
         id: 7,
-        name: 'cake',
+        name: 'wedding cake',
         image: 'images/cake-2.jpeg',
         price: 15,
         category: "cake"
     },
     {
         id: 8,
-        name: 'doughnut',
+        name: 'chocolate doughnut',
         image: 'images/doughnut-2.jpeg',
         price: 4,
         category: "doughnut"
     },
     {
         id: 9,
-        name: 'sweets',
+        name: 'fruit sweets',
         image: 'images/sweets-3.jpeg',
         price: 5,
         category: "sweets"
     },
     {
         id: 10,
-        name: 'cupcake',
+        name: 'blue cupcake',
         image: 'images/cupcake-3.jpeg',
         price: 12,
         category: "cupcake"
     },
     {
         id: 11,
-        name: 'cake',
+        name: 'pink cake',
         image: 'images/cake-3.jpeg',
         price: 30,
         category: "cake"
     },
     {
         id: 12,
-        name: 'doughnut',
+        name: 'design doughnut',
         image: 'images/doughnut-3.jpeg',
         price: 12,
         category: "doughnut"
     }
 ];
+
+const chefData = [
+    {
+        id: 1,
+        name: 'Bill Anderson',
+        image: 'images/team-1.jpg',
+    },
+    {
+        id: 2,
+        name: 'Peter Jones',
+        image: 'images/team-2.jpg',
+    },
+    {
+        id: 3,
+        name: 'Susan Smith',
+        image: 'images/team-3.jpg',
+    }
+]
--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-//add products to page
-let box = document.querySelector('.store .box');
-let addProducts = function (products) {
-    let draw = ''
-    products.forEach((product, key) => {
-        draw += `
-        <div class="col-md-4">
-            <div class="card mt-3 mb-3" id="${product.id}">
-                <div class="card-image">
-                    <img src=${product.image} class="card-img-top image" alt="image">
-                    <i class="fa-solid fa-cart-shopping cart-icon add-to-cart"></i>
-                </div>
-                <div class="card-body d-flex align-items-center justify-content-between">
-                    <h5 class="card-title text-uppercase">${product.name}</h5>
-                    <p class="card-text">$${product.price}</p>
-                </div>
-            </div>
-        </div>
-        `
-    })
-    box.innerHTML = draw
-}
-addProducts(products);
-
-
 //show cart 
 const cartInfo = document.getElementById('cart-info')
 const cart = document.getElementById('cart')
@@ -55,123 +30,114 @@ cards.forEach(card => {
     })
 })
 
-//create li in list
-const items = ['all', 'cakes', 'cupcakes', 'sweets', 'doughnuts']
+
+
+// display functions when page load 
+window.onload = function() {
+    displayLinks();
+    addProducts(products)
+    showChef(chefData)
+}
+
+
+//display category links in page
 const links = document.querySelector('.store .links')
-items.forEach((item) => {
-    const list = document.createElement('li');
-    list.classList.add('col')
-    list.setAttribute("data-filter", `.${item}`)
-    list.innerHTML = item
-    links.appendChild(list)
-})
+function displayLinks() {
+    const allCategories = products.map((item) => {
+        return item.category
+    })
+    allCategories.unshift('all')
+    const unquieCategories = new Set (allCategories)
+    let list = ''
+    unquieCategories.forEach(category => {
+        list += `
+            <li data-filter="${category}">${category}</li>
+        `
+    })
+    links.innerHTML = list
+    //select all links
+    const allLinks = document.querySelectorAll('.store .links li')
+    allLinks.forEach(link => {
+        link.onclick = function(e) {
+            const filterCategory = e.target.dataset.filter
+            const selectedCategory = products.filter(product => {
+                if(filterCategory === product.category) {
+                    return product
+                }
+            })
+            if(filterCategory === 'all') {
+                addProducts(products)
+            }else {
+                addProducts(selectedCategory)
+            }
+            //add active class
+            allLinks.forEach(link => {
+                link.classList.remove("active")
+            })
+            link.classList.add("active")
+        }
+    })
+}
 
 
-// let price = document.querySelector('.price')
-// let cards = []
-// function addToCart () {
-//     if(cards[key] == null){
-//         cards[key] = JSON.parse(JSON.stringify(products[key]));
-//     }
-//     reloadCard
-// }
-
-//add to cart 
-// const cartIcons = document.querySelectorAll('.cart-icon') 
-// console.log(cartIcons)
-// cartIcons.forEach(function(icon) {
-//     icon.addEventListener("click", function(event){
-//         console.log(event.target)
-//         // console.log(cartIcon.previousElementSibling.src.indexOf('img'))
-//     })
-// });
-
-// function getpath (e) {
-//     console.log(e.target)
-//     console.log(cartIcon.previousElementSibling.src.indexOf('img'))
-// }
-
-// // let card = document.querySelector('.card')
-// // for( i of products){
-// //     console.log(i.category)
-// //     cards.forEach(card => {
-// //             card.classList.add(i.category)        
-// //     })
-// // }
-
-// // let cards = document.querySelectorAll('.card')
-// // cards.forEach(card => {
-// //     products.forEach(product => {
-// //         card.classList.add(product.category)
-// //     })
-    
-// // })
-
-// // products.forEach(product => {
-// //     // console.log(product.category)
-// //     cards.forEach(card => {
-// //         // console.log(product)
-// //         card.classList.add(product.category)
-// //     })
-// // })
-// // let cards = document.querySelectorAll('.card');
-// // cards.forEach(product => {
-// //     console.log(product)
-// //     cat = product.classList.add("category")
-// // })
+//add products to page
+const productsBox = document.querySelector('.store .box');
+function addProducts(products) {
+    let drawProducts = ''
+    products.forEach((product) => {
+        drawProducts += `
+            <div class="col-md-4">
+                <div class="card mt-3 mb-3" id="${product.id}">
+                    <div class="card-image">
+                        <img src=${product.image} class="card-img-top image" alt="image">
+                        <i class="fa-solid fa-cart-shopping cart-icon add-to-cart"></i>
+                    </div>
+                    <div class="card-body d-flex align-items-center justify-content-between">
+                        <h5 class="card-title text-uppercase">${product.name}</h5>
+                        <p class="card-text">$${product.price}</p>
+                    </div>
+                </div>
+            </div>
+        `
+    })
+    productsBox.innerHTML = drawProducts
+}
 
 
 
-// //filter products
-// let cards = document.querySelectorAll('.card');
-// // function filterProducts (value) {
-// //     btns.forEach(btn => {
-// //         if(value.toUpperCase() === btn.innerText.toUpperCase()) {
-// //             btn.classList.add('active')
-// //         }else {
-// //             btn.classList.remove('active')
-// //         }
-// //     })
-
-// //     cards.forEach(card => {
-// //         card.classList.add('hide')
-// //         if(value == 'all'){
-// //         card.classList.remove('hide')
-// //         }else {
-// //             if(card.classList.contains(value)) {
-// //                 card.classList.remove('hide')
-// //             }else {
-// //                 card.classList.remove('hide')
-// //             }
-// //         }
-// //     })
-    
-// // }
-
-// // window.onload = () => {
-// //     filterProducts('all')
-// // }
-
-// cards.forEach(card=> {
-//     card.classList.add('hide')
-// })
-// //search products
-// let cardsTitle = document.querySelectorAll('.card-title')
-// let searchBtn = document.querySelector('.search')
-// let inputValue = document.querySelector('#item').value;
-
-// searchBtn.addEventListener('click', searchProducts)
-
-// function searchProducts (e) {
-//     e.preventDefault()
-//     // cardsTitle.forEach((item,index) => {
-//     //     if(item.innerText.includes(inputValue.toUpperCase())){
-//     //         cards[index].classList.remove('hide')
-//     //     }else {
-//     //         cards[index].classList.add('hide')
-//     //     }
-//     // })
-    
-// }
+//filter products to search
+const searchBtn = document.querySelector('.search')
+const input = document.querySelector('.input')
+searchBtn.addEventListener('click', search)
+function search() {
+    const value = input.value
+    if(value !== "") {
+        const filteredProducts = products.filter(product => {
+            return product.name.includes(value) 
+        })
+        addProducts(filteredProducts)
+    }
+}
 
 
+
+//function to display chef data
+const chefBox = document.querySelector('.chef .row')
+function showChef(data) {
+    let display = ''
+    data.map(chef => {
+        display += `
+        <div class="col-md-4">
+            <div id="${chef.id}" class="box mb-4 position-relative overflow-hidden rounded">
+                <img src="${chef.image}" alt="image" class="img-fluid">
+                <div class="icons">
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+        `
+    })
+    chefBox.innerHTML = display
+}
